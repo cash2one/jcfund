912cf2e5f4976555b783edcc1a77ce4a2000391b
Yang Hua <huawise@gmail.com>
2016-03-08 18:12:20 +0800

    jcfund

---
+++ data/common.inc.php
@@ -0,0 +1,11 @@
+<?php
+//数据库连接信息
+$cfg_dbhost = 'localhost';
+$cfg_dbname = 'jcfund';
+$cfg_dbuser = 'jcfund';
+$cfg_dbpwd = 'JCfund168';
+$cfg_dbprefix = 'dede_';
+$cfg_db_language = 'utf8';
+
+
+?>
